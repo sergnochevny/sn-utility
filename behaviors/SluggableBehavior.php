ce08d2cae7e7559ee239534c50a2a6ba298252f9
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-04-12 10:09:36 +0300

    add SluggableBehavior 1.0.3

---
+++ behaviors/SluggableBehavior.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Date: 16.03.2017
+ * Time: 20:15
+ */
+
+namespace ait\utilities\behaviors;
+
+use yii\behaviors\SluggableBehavior as yiiSluggable;
+
+class SluggableBehavior extends yiiSluggable
+{
+
+    /**
+     * @inheritdoc
+     */
+    protected function generateSlug($slugParts)
+    {
+        $out = parent::generateSlug($slugParts);
+        $out = preg_replace('/[^a-zA-Z0-9]+/i', ' ', $out);
+        $out = trim(preg_replace('/\s{2,}/', ' ', $out));
+        $out = str_replace(' ', '-', $out);
+        return  strtolower($out);
+    }
+
+}
