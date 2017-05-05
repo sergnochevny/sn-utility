cafbe5084dfd1dd6663b51956957e60b58710e0a
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-05-05 20:53:21 +0300

    Add some comments      1.0.20

--- behaviors/ChildexistsBehavior.php
+++ behaviors/ChildexistsBehavior.php
@@ -8,21 +8,39 @@ use yii\base\InvalidConfigException;
 use yii\db\ActiveRecord;
 use yii\validators\ExistValidator;
 
+/**
+ * Class ChildexistsBehavior
+ * @package ait\utilities\behaviors
+ *
+ *    public function behaviors()
+ *   {
+ *      return [
+ *          ['class' => TimestampBehavior::className()],
+ *          [
+ *              'class' => ChildexistsBehavior::className(),
+ *              'parent' => 'id',
+ *              'model_class' => Customers::className(),
+ *              'child' => 'user_id',
+ *              'message' => 'Deleting is not possible - there are dependent customers.',
+ *          ],
+ *      ];
+ *   }
+ *
+ */
 class ChildexistsBehavior extends Behavior
 {
 
+    private $beforeDelAction = 'beforeDelAction';
     public $parent;
     public $child;
     public $message;
     public $model_class;
 
-    private $beforeDelAction = 'beforeDelAction';
-
     public function init()
     {
         parent::init();
 
-        if(is_null($this->parent) || is_null($this->child) || is_null($this->message)){
+        if (is_null($this->parent) || is_null($this->child) || is_null($this->message)) {
             throw new InvalidConfigException('parent, child & message  parameters must be identity');
         }
     }
@@ -35,11 +53,12 @@ class ChildexistsBehavior extends Behavior
 
     }
 
-    public function beforeDelAction($event){
+    public function beforeDelAction($event)
+    {
         $model = clone $this->owner;
         $validator = Yii::createObject(ExistValidator::className());
-        $validator->targetAttribute = [$this->parent=>$this->child];
-        if(!is_null($this->model_class)){
+        $validator->targetAttribute = [$this->parent => $this->child];
+        if (!is_null($this->model_class)) {
             $validator->targetClass = new $this->model_class;
         }
         $validator->validateAttribute($model, $this->parent);
@@ -47,7 +66,7 @@ class ChildexistsBehavior extends Behavior
         $event->handled = !$res;
         $event->isValid = $res;
 
-        if (!$res){
+        if (!$res) {
             Yii::$app->session->setFlash('alert', [
                 'body' => $this->message,
                 'options' => ['class' => 'alert-danger'],
