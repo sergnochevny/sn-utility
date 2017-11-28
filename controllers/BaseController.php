738eef2b1126c9c217a2b36db3de70e539eb79c5
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-11-28 18:11:27 +0200

    add BaseController       1.0.50

---
+++ controllers/BaseController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ait\utility\controllers;
+
+use yii\web\Controller;
+
+class BaseController extends Controller
+{
+    /**
+     * @param string $view
+     * @param array $params
+     * @return string
+     */
+    protected function smartRender($view, $params = [])
+    {
+        if (\Yii::$app->getRequest()->getIsAjax()) {
+            return $this->renderAjax($view, $params);
+        }
+
+        return $this->render($view, $params);
+    }
+}
